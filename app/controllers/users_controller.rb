f919b018e05d1332d5dff4bf5cd85ce884ef329e
christiegoldstein <cngold15@gmail.com>
2016-07-27 17:14:00 -0400

    fixed all routing issues, added nav bar to most pages once logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,6 @@ class UsersController < ApplicationController
 
 	def edit
 		@user = current_user
-		#@user.hobbies.build
-		#@user.courses.build
 		render "edit"
 	end
 
@@ -22,12 +20,6 @@ class UsersController < ApplicationController
 		@user = current_user
 		@course = Course.find_by(name: params[:user][:courses_attributes]["0"][:name])
 		@hobby = Course.find_by(name: params[:user][:hobbies_attributes]["0"][:name])
-		puts @hobby
-		if user_params[:password].blank?
-    		user_params.delete(:password)
-    		user_params.delete(:password_confirmation)
- 		end
-
 
  		if @course != nil
 		  	UserCourse.create!(user_id: @user.id, course_id: @course.id)
@@ -41,13 +33,13 @@ class UsersController < ApplicationController
 			Hobby.create!(name: params[:user][:hobbies_attributes][:name])		
 		end
 
-	    if @user.update_attributes(user_params)
-	      flash[:notice] = 'The User is successfully updated!'
-	      redirect_to "/users/profile"
-	    else
-	      flash[:error] = @user.errors.full_messages[0]
-	      redirect_to "/users/profile/edit"
-	    end
+		params[:user].delete(:password) if params[:user][:password].blank?
+		if @user.update_attributes(user_params)
+		   flash[:success] = "Edit Successful."
+		   redirect_to '/users/profile'
+		else
+		   render 'edit'
+		end
 	end
 
 	private
