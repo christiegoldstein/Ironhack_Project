a7b0cbe09cd4bb9a23d4f1af26020f7cdb31849a
christiegoldstein <cngold15@gmail.com>
2016-08-03 19:38:33 -0400

    fixed migrations for datetime

--- db/migrate/20160725150516_change_meet_time_column_to_time.rb
+++ db/migrate/20160725150516_change_meet_time_column_to_time.rb
@@ -1,5 +1,6 @@
 class ChangeMeetTimeColumnToTime < ActiveRecord::Migration[5.0]
   def change
-  	change_column :study_groups, :meet_time, :datetime
+  	remove_column :study_groups, :meet_time
+  	add_column :study_groups, :meet_time, :datetime
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160725153545) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "courses", force: :cascade do |t|
     t.string   "name"
     t.string   "code"
@@ -74,8 +77,8 @@ ActiveRecord::Schema.define(version: 20160725153545) do
     t.string   "minor"
     t.string   "last_name"
     t.integer  "age"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
