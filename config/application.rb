7e6541756c70eb5f885b5f1e9698968cdc4424f8
christiegoldstein <cngold15@gmail.com>
2016-08-03 20:03:56 -0400

    assets to true

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module MyProject
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
-    config.serve_static_assets = true
 
   end
 end
